d0e9c9482dcc67b5a0152a5cc81d06c664f62b3e
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-20 15:12:08 +0530

    add onchange, emojilist, onclick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+const emojiDictionary = {
+  "🎅": "santa claus",
+  "🎄": "christmas tree",
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+
+const emojilist = Object.keys(emojiDictionary);
+
 export default function App() {
+  const [emojiMeaningOutput, setEmojiMeaningOutput] = useState(
+    "translation will appear here"
+  );
+  const [emoji, setEmoji] = useState("");
+  function onChangeHandler() {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+    if (inputEmoji in emojiDictionary) {
+      setEmojiMeaningOutput(emojiDictionary[inputEmoji]);
+    } else {
+      setEmojiMeaningOutput("falure to recognize this emoji");
+    }
+  }
+  function emojiClickHandler(inputEmoji) {
+    setEmoji(emoji);
+    setEmojiMeaningOutput(emojiDictionary[inputEmoji]);
+  }
+
   return (
     <div className="App">
       <h1>inside Out!!</h1>
-      <input />
+      <input onChange={onChangeHandler} />
+      <div>{emoji}</div>
+      <div>{emojiMeaningOutput}</div>
+      {emojilist.map((emoji) => (
+        <span onClick={() => emojiClickHandler(emoji)}> {emoji} </span>
+      ))}
     </div>
   );
 }
