1d48768b9240ddfe57a9c966a4520c920534719c
Varen Aggarwal <varenaggarwal@gmail.com>
2020-12-20 14:31:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>inside Out!!</h1>
+      <input />
+    </div>
+  );
+}
